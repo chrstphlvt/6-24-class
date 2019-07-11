3e333e1ec9d0b973d848b1f224f0255f755990b8
Blake <btkruppa513@gmail.com>
2019-07-11 15:30:18 -0400

    added a couple pieces of middleware to express template

--- week3/express-template - Copy/src/index.ts
+++ week3/express-template - Copy/src/index.ts
@@ -5,26 +5,12 @@ const port = 8012;
 const app = express();
 
 /**
- * Loggin middleware
  * This callback will be invoked anytime a request is made 
  * regardless of url or http method
  *  */ 
-app.use((req, res, next) => {
+app.use((req, res) => {
     console.log(`request made with url: ${req.url} and method ${req.method}`)
-    next(); // pass request on to search for the next piece of middleware
-});
-
-app.get('/test', (req, res) => {
-    res.send('this is a test endpoint');
-});
-
-app.get('/data', (req, res) => {
-    res.json({
-        name: 'Island',
-        quality: 'new',
-        price: '0.15',
-        game: 'MTG'
-    });
+    res.send('hello');
 });
 
 
